18a9f2a1eeb08760fec3dfd14cbb499948af8961
Faraz Khan <farazrkhan@gmail.com>
2022-07-21 12:48:34 -0700

    - Better styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { SignInButton } from '../components'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <div>
-      BOO
+      4H20 is awesome!
+      Check out the <Link href='/profile'>profile</Link> page!
     </div>
   )
 }
